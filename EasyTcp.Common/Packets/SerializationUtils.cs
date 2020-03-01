32e760246a3b4c218908d13e92a771c5bb231322
Unknown <38399178+Awware@users.noreply.github.com>
2020-03-01 15:31:18 +0600

    Simple serialization util

---
+++ EasyTcp.Common/Packets/SerializationUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTcp.Common.Packets
+{
+    public static class SerializationUtils
+    {
+        public static T FromBytes<T>(byte[] bytes)
+        {
+            using (MemoryStream mem = new MemoryStream(bytes))
+            {
+                SoapFormatter Soap = new SoapFormatter();
+                return (T)Soap.Deserialize(mem);
+            }
+        }
+        public static byte[] ToBytes(object obj)
+        {
+            using (MemoryStream mem = new MemoryStream())
+            {
+                SoapFormatter Soap = new SoapFormatter();
+                Soap.Serialize(mem, obj);
+                return mem.ToArray();
+            }
+        }
+    }
+}
